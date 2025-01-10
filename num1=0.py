126623adda2e3a801b6cb096b29852bd6c7ca92e
manugalvez <manuelgalveznahuel@gmail.com>
2025-01-09 21:40:39 -0300

    Add files via upload

---
+++ num1=0.py
@@ -0,0 +1,114 @@
+#def suma()
+#    print("Ingrese número")
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+#
+#def resta()
+#    print("Ingrese número")
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+#
+#def multiplicación()
+#    print("Ingrese número")        
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+#
+#def división()
+#   print("Ingrese número")
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+
+#num1=0
+#um2=0
+#suma=1
+#esta=2
+#ultiplicación=3
+#división=4
+#print("Elija una opceración matematica")
+#print("""-suma=1
+#-resta=2
+#-multiplicación=3
+#-división=4"""
+#)
+#op=int(input())
+#if op==(1):
+#    print("Ingrese número")
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+#    resultado=num1+num2
+#    print("El resultado es: ", resultado)
+#elif(op==(2)):
+#    print("Ingrese número")
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+#    resultado=num1-num2
+#    print("El resultado es: ", resultado)
+#elif(op==(3)):
+#    print("Ingrese número")        
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+#    resultado=num1*num2
+#    print("El resultado es: ", resultado)
+#elif(op==(4)):
+#    print("Ingrese número")
+#    num1=int(input())
+#    print("Ingrese otro número")
+#    num2=int(input())
+#    resultado=num1/num2
+#    print("El resultado es:", resultado)
+#else:
+#    print("Escoja opción valida")
+
+
+#num=9
+#def ver_par():
+#    if num>0 and num%2==0:
+#        print("El número es positivo y par")
+#    else:
+#        print("El número es positivo y no es par")
+
+#def primerwhile():
+#texto=""
+
+#while texto !="hola":
+#     print("Ingrese saludo correcto")
+#     texto=input()
+#print("Ese es el saludo correcto")
+        
+#num=0
+        
+#while num <=3:
+#      num=num+1
+#      print("Iteración", num)
+
+#comida = 100
+#comio = 25
+#while comida != True:
+#    print("¿Va a comer? si/no")
+#    respuesta=input()
+#    if respuesta == "no":
+#        comida = 100
+#        comida = True
+#    elif respuesta == "si":
+#        cantidad = comida-comio
+#        print("Le queda", cantidad)
+#        comida = True
+#    else:
+#        print("Aun le queda comida", cantidad)    
+    
+# clave=""
+# print("Bienvenido a la computadora central de Skynet, ingrese la clave")
+# clave=input()
+
+# while clave !="1997":
+#         print("Su clave es incorrecta")
+#         clave=input()
+# print("A ingresado con exito")
+
